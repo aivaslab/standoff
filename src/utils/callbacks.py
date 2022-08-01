379e606e02d1628f94f2277aeca22cd5a8ba6e23
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-08-01 15:25:14 -0500

    Update callbacks.py

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -38,8 +38,10 @@ class PlottingCallback(BaseCallback):
 
     def _on_step(self) -> bool:
         with open(self.logPath, 'a') as logfile:
-            logfile.write('accuracies and stuff\n')
-            #todo: add custom cb for special infos (e.g. avoidance%, bigR%)
+
+            logfile.write('ts: ', self.eval_cbs[0].evaluations_timesteps[-1], 
+                str([name, np.mean(x.evaluations_results[-1]) for name, x in zip(self.names, self.eval_cbs)]), 
+                '\n')
 
         plot_evals(self.savePath, self.name, self.names, self.eval_cbs)
         for env, name in zip(self.envs, self.names):
@@ -50,7 +52,7 @@ class PlottingCallbackStartStop(BaseCallback):
     """
     # bandaid fix to EveryNTimesteps not triggering at training start and end
     """
-    def __init__(self, verbose=0, savePath='', name='', envs=[], names=[], eval_cbs=[], params=[]):
+    def __init__(self, verbose=0, savePath='', name='', envs=[], names=[], eval_cbs=[], params=[], model=None):
         super().__init__(verbose)
         self.savePath = savePath
         self.logPath = os.path.join(savePath, 'logs.txt')
@@ -60,6 +62,7 @@ class PlottingCallbackStartStop(BaseCallback):
         self.eval_cbs = eval_cbs
         self.start_time = 0
         self.params = params
+        self.model = model
 
     def _on_training_start(self) -> bool:
         super()._on_training_start()
@@ -67,6 +70,7 @@ class PlottingCallbackStartStop(BaseCallback):
         with open(self.logPath, 'a') as logfile:
             logfile.write(self.params)
             logfile.write("\n")
+            logfile.write(str(self.model.policy))
         plot_evals(self.savePath, self.name, self.names, self.eval_cbs)
         for env, name in zip(self.envs, self.names):
             make_pic_video(self.model, env, name, False, True, self.savePath)
