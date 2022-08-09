280dbce8ff364f6e13fd910942ce54b1bb0fc370
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-08-09 13:19:24 -0500

    Update conversion.py

--- src/utils/conversion.py
+++ src/utils/conversion.py
@@ -51,7 +51,8 @@ def make_env(envClass, player_config, configName=None, memory=1, threads=1, redu
     if reduce_color:
         env = ss.reshape_v0(env, (size, size, 1))
     env = ss.pettingzoo_env_to_vec_env_v1(env)
-    env = ss.concat_vec_envs_v1(env, threads, num_cpus=2, base_class='stable_baselines3')
+    env = ss.concat_vec_envs_v1(env, threads, num_cpus=1, base_class='stable_baselines3')
+    # num_cpus=1 changed from 2 to avoid csv issues. does it affect speed?
     env = VecTransposeImage(env)
     if memory > 1:
         env = VecFrameStack(env, n_stack=memory)
