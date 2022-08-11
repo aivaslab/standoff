5601959ec817718aa5ff0e2770d3726c9e4698e5
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-08-11 12:44:47 -0500

    Update base_AEC.py

--- src/base_AEC.py
+++ src/base_AEC.py
@@ -549,8 +549,6 @@ class para_MultiGridEnv(ParallelEnv):
         self.env_done = False
 
         if hasattr(self, "hard_reset"):
-            print(self.configName)
-            print(self.configs)
             self.hard_reset(self.configs[self.configName])
         else:
             print("No hard reset function found")
