18d76c7cd7f86d12e027becec957f9edc9612127
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-09-28 16:56:03 -0500

    Update callbacks.py

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -14,7 +14,7 @@ class TrainUpdateCallback(BaseCallback):
         self.batch_size = batch_size
     def _on_rollout_end(self):
         for env in self.envs:
-            env.unwrapped().minibatch += self.batch_size
+            env.unwrapped.vec_envs[0].par_env.minibatch += self.batch_size
     def _on_step(self):
         return True
 
