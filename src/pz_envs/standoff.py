b27098c46d7fc95e2a7634267b4647d318cb23cb
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-10-10 15:11:37 -0500

    add deterministic generation for evals

--- src/pz_envs/standoff.py
+++ src/pz_envs/standoff.py
@@ -48,6 +48,7 @@ class StandoffEnv(para_MultiGridEnv):
         self.params = None
         self.configName = configName
         self.minibatch = 0
+        self.deterministic = False #used for generating deterministic levels for ground-truth evaluation
 
     def hard_reset(self, params=None):
         """
@@ -187,15 +188,15 @@ class StandoffEnv(para_MultiGridEnv):
             type = event[0]
             for x in range(len(event)):
                 if event[x] == "empty":
-                    event[x] = empty_buckets.pop(random.randrange(len(empty_buckets)))
+                    event[x] = empty_buckets.pop(random.randrange(len(empty_buckets)) if self.deterministic else 0)
                 elif event[x] == "else":
                     available_spots = [i for i in range(boxes) if i != event[x - 1]]
-                    event[x] = available_spots.pop(random.randrange(len(available_spots)))
+                    event[x] = available_spots.pop(random.randrange(len(available_spots)) if self.deterministic else 0)
                 elif isinstance(event[x], int):
                     event[x] = events[event[x]][1]  # get first location
 
             if type == "bait":
-                event_args[k] = bait_args.pop(random.randrange(len(bait_args)))
+                event_args[k] = bait_args.pop(random.randrange(len(bait_args)) if self.deterministic else 0)
             elif type == "remove":
                 empty_buckets.append(event[1])
             elif type == "obscure" or type == "reveal":
