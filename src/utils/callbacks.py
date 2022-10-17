dafceb493db4c58b1455949babbac68b79a964c6
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-10-17 12:36:38 -0500

    Update callbacks.py

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -207,7 +207,7 @@ class PlottingCallback(BaseCallback):
             # logfile.write(f'ts: {self.eval_cbs[0].evaluations_timesteps[-1]}\tkl: {self.model.approxkl}\n')
 
         #plot_evals(self.savePath, self.name, self.names, self.eval_cbs)
-        self.eval_df.append( ground_truth_evals(self.envs, self.model), ignore_index=True)
+        self.eval_df = self.eval_df.append( ground_truth_evals(self.envs, self.model), ignore_index=True)
         plot_evals_df(self.eval_df, self.savePath, self.name)
 
         if self.mid_vids:
@@ -265,7 +265,7 @@ class PlottingCallbackStartStop(BaseCallback):
             logfile.write("\n")
             logfile.write(str(self.model.policy))
         #plot_evals(self.savePath, self.name, self.names, self.eval_cbs)
-        self.eval_df.append( ground_truth_evals(self.envs, self.model), ignore_index=True)
+        self.eval_df = self.eval_df.append( ground_truth_evals(self.envs, self.model), ignore_index=True)
         plot_evals_df(self.eval_df, self.savePath, self.name)
         if not os.path.exists(os.path.join(self.savePath, 'videos')):
             os.mkdir(os.path.join(self.savePath, 'videos'))
@@ -289,7 +289,7 @@ class PlottingCallbackStartStop(BaseCallback):
                 logfile.write('end of training! total time:' + str(time.time() - self.start_time) + '\n')
 
             #plot_evals(self.savePath, self.name, self.names, self.eval_cbs)
-            self.eval_df.append( ground_truth_evals(self.envs, self.model), ignore_index=True)
+            self.eval_df = self.eval_df.append( ground_truth_evals(self.envs, self.model), ignore_index=True)
             plot_evals_df(self.eval_df, self.savePath, self.name)
 
             update_global_logs(self.global_log_path, self.log_line, {
