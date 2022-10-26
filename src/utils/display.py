221199012bc251629eab37e252031eb657fc5d80
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-10-26 11:53:39 -0500

    Update display.py

--- src/utils/display.py
+++ src/utils/display.py
@@ -248,7 +248,7 @@ def plot_evals_df(df, savePath, name):
     fig, axs = plt.subplots(1)
     unique_names = df.configName.unique()
     for cf in unique_names:
-        df2 = df[df['configName'] == cf]
+        df2 = df[df['configName'] == cf].groupby('minibatch', as_index=False).mean().sort_values('minibatch')
         plt.plot(df2.minibatch, df2['accuracy-c'], label=cf, )
     plt.legend(bbox_to_anchor=(1, 1), loc="upper left")
     plt.title(name)
