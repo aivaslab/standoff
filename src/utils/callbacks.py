028821e37332f3698f9b4dcf05b7ccaa1fc616c2
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-11-01 16:39:55 -0500

    Update callbacks.py

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -134,7 +134,7 @@ def ground_truth_evals(eval_envs, model, repetitions=25, memory=1):
 
                     obs = torch.from_numpy(obs['player_0']).swapdims(0, 2).unsqueeze(0)
                     print('debug_shape1', obs.shape)
-                    remembered_obs = torch.stack([obs, remembered_obs[:, :-1, :, :]], dim=1).reshape(obs_shape)
+                    remembered_obs = torch.stack([obs, remembered_obs[:, -memory:, :, :]], dim=1).reshape(obs_shape)
                     print('debug_shape2', remembered_obs.shape)
                     cur_obs = remembered_obs[-memory:]
                     print('debug_shape3', cur_obs.shape)
