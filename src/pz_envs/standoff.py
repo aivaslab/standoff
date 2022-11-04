2c097f0332b99c3d5366b1043befbb0d99716d1e
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-11-03 22:44:24 -0500

    Update standoff.py

--- src/pz_envs/standoff.py
+++ src/pz_envs/standoff.py
@@ -15,7 +15,7 @@ class StandoffEnv(para_MultiGridEnv):
     info_keywords = ('minibatch', 'timestep',
                      'shouldAvoidBig', 'shouldAvoidSmall', 'correctSelection', 'selection',
                     'selectedBig', 'selectedSmall', 'selectedNeither',
-                    'selectedPrevBig', 'selectedPrevSmall', 'selectedPrevNeither', 'incorrectSolution')
+                    'selectedPrevBig', 'selectedPrevSmall', 'selectedPrevNeither', 'incorrectSelection')
 
     def __init__(
             self,
