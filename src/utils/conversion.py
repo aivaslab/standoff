724272586056588f052366b459d7c18af50e1979
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-11-08 15:00:21 -0600

    Update conversion.py

--- src/utils/conversion.py
+++ src/utils/conversion.py
@@ -51,6 +51,7 @@ def make_env(envClass, player_config, configName=None, memory=1, threads=1, redu
     env = ss.resize_v0(env, x_size=size, y_size=size)
     if reduce_color:
         env = ss.reshape_v0(env, (size, size, 1))
+    env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
     env = ss.pettingzoo_env_to_vec_env_v1(env)
     env = ss.concat_vec_envs_v1(env, threads, num_cpus=1, base_class='stable_baselines3')
     # num_cpus=1 changed from 2 to avoid csv issues. does it affect speed?
@@ -66,7 +67,6 @@ def make_env(envClass, player_config, configName=None, memory=1, threads=1, redu
             env = VecMonitor(env, filename=os.path.join(path, f"{configName}-{rank}"), info_keywords=info_keywords)
         else:
             env = VecMonitor(env, filename=f"{configName}-{rank}", info_keywords=info_keywords)
-    env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
     return env
 
 def wrap_env(para_env, **kwargs):
