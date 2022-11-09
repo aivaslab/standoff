1542fc904477907eb79544af1d82abe9039e8f5f
Joel Michelson <joelmichelson@users.noreply.github.com>
2022-11-09 12:12:02 -0600

    Update conversion.py

--- src/utils/conversion.py
+++ src/utils/conversion.py
@@ -55,7 +55,7 @@ def make_env(envClass, player_config, configName=None, memory=1, threads=1, redu
     env = ss.pettingzoo_env_to_vec_env_v1(env)
     env = ss.concat_vec_envs_v1(env, threads, num_cpus=1, base_class='stable_baselines3')
     # num_cpus=1 changed from 2 to avoid csv issues. does it affect speed?
-    if rank == 0:
+    if rank == 0 and False:
         env = VecNormalizeMultiAgent(env, norm_obs=True, norm_reward=False, clip_obs=10.)
     env = VecTransposeImage(env)
     if memory > 1:
