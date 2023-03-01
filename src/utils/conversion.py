40cbf239848049651f478df493694f41b8a595a1
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-03-01 15:02:43 -0600

    Update conversion.py

--- src/utils/conversion.py
+++ src/utils/conversion.py
@@ -37,6 +37,7 @@ def make_env_comp(env_name, frames=1, vecNormalize=False, size=32, style='rich',
     env = wrap_env_full(env.env, memory=frames, size=size,
                         vecNormalize=vecNormalize,
                         style=style, monitor_path=monitor_path, rank=rank)
+    print('monitor path:', os.path.join(monitor_path, f'{env_name}-{rank}'))
     env = VecMonitor(env, os.path.join(monitor_path, f'{env_name}-{rank}')) # should get info keywords here for train monitoring, eg accuracy
     env.rank = rank
     return env
