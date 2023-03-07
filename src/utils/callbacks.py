cb3cde271df12b5a424dd1aa860fd29c3f4898f5
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-03-07 14:38:51 -0600

    Update callbacks.py

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -173,8 +173,9 @@ def collect_rollouts(env, train_env, model, model_episode, episodes=100, memory=
             cur_obs = np.array(cur_obs)'''
 
             # swap dims, add batch dim... unclear if dim swap has parity with wrapped env
-            obs = np.expand_dims(np.array(obs['player_0']).swapaxes(0, 1), 0).swapaxes(3, 4)
-            #print('obs2', obs.shape) # (1, 5, 17, 17)
+            obs = np.expand_dims(np.array(obs['player_0']).swapaxes(0, 1), 0)
+            print('obs2', obs.shape) # (1, 5, 17, 17)
+            obs = obs.swapaxes(2, 3) # (1, 5, 17, 17)
             #remembered_obs = np.concatenate([obs, remembered_obs], axis=1)
             #cur_obs = remembered_obs[:, -memory * channels:, :, :]
             #print('cur_obs', cur_obs.shape)
