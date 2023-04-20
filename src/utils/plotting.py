47aec26019cbe10bb01b237a7dbbffe67dfd52ba
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-04-20 10:08:04 -0500

    Update plotting.py

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -178,9 +178,9 @@ def plot_train_many(train_paths, window=1000, path=None):
                 if len(df):
                     df['index_col'] = df.index
                     df['yrolling'] = df['r'].rolling(window=window).mean()
-                    plt.plot(df.index, df.yrolling, label=log_folder)
+                    plt.plot(df.index, df.yrolling, label=os.path.basename(log_folder))
 
-    plt.rcParams["figure.figsize"] = (10, 5)
+    plt.rcParams["figure.figsize"] = (15, 5)
     plt.xlabel('Episode')
     plt.ylabel('Reward')
     plt.title('Learning curve')
