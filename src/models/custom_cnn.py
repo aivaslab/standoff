5e3f0f5c66797937788af48b17426352548a0d48
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-05-03 14:37:07 -0500

    remove padding from cnn

--- src/models/custom_cnn.py
+++ src/models/custom_cnn.py
@@ -19,9 +19,9 @@ class CustomCNN(BaseFeaturesExtractor):
         n_input_channels = self.get_image_obs(observation_space).shape[0]
         #print(self.get_image_obs(observation_space).shape)
         self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 4*conv_mult*frames, kernel_size=5, stride=2, padding=1),
+            nn.Conv2d(n_input_channels, 4*conv_mult*frames, kernel_size=5, stride=2, padding=0),
             nn.ReLU(),
-            nn.Conv2d(4*conv_mult*frames, 8*conv_mult, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(4*conv_mult*frames, 8*conv_mult, kernel_size=3, stride=2, padding=0),
             nn.ReLU(),
             nn.Flatten(),
         )
