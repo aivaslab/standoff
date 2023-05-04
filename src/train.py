a5aff44ab4df12148680f0bde679760f04e930c4
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-05-04 01:56:19 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -149,6 +149,7 @@ def main(args):
                 norm_rewards = args.norm_rewards
 
                 print('hidden_size', hidden_size)
+                print('schedule', args.schedule)
 
                 env_name = f"Standoff-{env_name_temp}-{str(size)}-{style}-{str(difficulty)}-v0"
                 savePath3 = os.path.join(train_path_ext, env_name + name)
