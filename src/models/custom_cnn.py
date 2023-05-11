e3a82612c95d17a8b305a4cf8e13f6b8c287369c
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-05-11 15:38:53 -0500

    sl module loading fix

--- src/models/custom_cnn.py
+++ src/models/custom_cnn.py
@@ -17,7 +17,8 @@ class CustomCNN(BaseFeaturesExtractor):
         # We assume CxHxW images (channels first)
         # Re-ordering will be done by pre-preprocessing or wrapper
         n_input_channels = self.get_image_obs(observation_space).shape[0]
-        self.n_label_channels = self.get_label_obs(observation_space).shape[0] if use_label else 0
+        self.n_label_channels = len(self.get_label_obs(observation_space)) if use_label else 0
+        print('n_label_channels', self.n_label_channels)
         #print(self.get_image_obs(observation_space).shape)
         self.cnn = nn.Sequential(
             nn.Conv2d(n_input_channels, 4*conv_mult*frames, kernel_size=5, stride=2, padding=0),
