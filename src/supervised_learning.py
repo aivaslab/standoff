94c2b6a10a8d0cd1f755566e3530735206b73e17
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-05-13 22:55:41 -0500

    Update supervised_learning.py

--- src/supervised_learning.py
+++ src/supervised_learning.py
@@ -161,7 +161,9 @@ class RNNModel(nn.Module):
 
     def forward(self, x):
         #x = x.view((-1, 10, 6, 17, 17))
-        print(x.shape)
+        #print(x.shape)
+        # convert x to tensor
+        x = torch.tensor(x, dtype=torch.float32)
         conv_outputs = []
         for t in range(10):
             x_t = x[:, t, :, :, :]
