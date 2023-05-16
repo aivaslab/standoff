884f6c704e1505a645959a5ae6a5158c7e4e5730
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-05-15 21:36:42 -0500

    Update custom_cnn.py

--- src/models/custom_cnn.py
+++ src/models/custom_cnn.py
@@ -37,10 +37,12 @@ class CustomCNN(BaseFeaturesExtractor):
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         image_obs = self.get_image_obs(observations)
+        print(image_obs.shape)
         features = self.cnn(image_obs[: -1 if self.label_dim > 0 else None])
+        print(features.shape)
         if self.label_dim > 0:
-            labels = image_obs[-1].unsqueeze(0)
-            
+            labels = image_obs[-1]
+
             features = th.cat([features, labels], dim=1)
 
         return self.linear(features)
