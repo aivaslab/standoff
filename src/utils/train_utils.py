542322d2bbc8577bacf2322e572c926083920361
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-05-24 12:17:47 -0500

    momentum

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -24,6 +24,7 @@ def init_policy(model_class, obs_space, act_space, lr_schedule, width, hidden_si
             'normalize_images': normalize_images,
             'activation_fn': th.nn.ReLU,
             'optimizer_class': th.optim.SGD,
+            'optimizer_kwargs': {'momentum': 0.9, 'nesterov': True},
             'features_extractor_kwargs': {
                 'features_dim': width,
                 'conv_mult': conv_mult,
