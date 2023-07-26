351d862c20c20334ebf1c50700c656d74d4dcee6
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-07-26 13:34:01 -0500

    Update scenario_configs.py

--- src/pz_envs/scenario_configs.py
+++ src/pz_envs/scenario_configs.py
@@ -2,12 +2,11 @@ from itertools import product, combinations
 
 import numpy as np
 
-
 def generate_fillers(timesteps_to_fill, fill_len):
     positions = list(range(1, timesteps_to_fill))
     for c in combinations(positions, fill_len - 1):
         split_positions = [0] + list(c) + [timesteps_to_fill]
-        solution = [split_positions[i + 1] - split_positions[i] for i in range(fill_len)]
+        solution = [split_positions[i+1] - split_positions[i] for i in range(fill_len)]
         yield solution
 
 
@@ -25,7 +24,6 @@ def parameter_generator(space, params={}):
             yield from parameter_generator(new_space, new_params)
     else:
         yield params
-
 
 def count_non_ob_re(data):
     count = sum(1 for item in data if item not in [['ob'], ['re']])
@@ -74,7 +72,6 @@ def remove_unnecessary_sequences(events):
 
     return events
 
-
 def count_permutations(event_list):
     permutations = []
     num_locations = 5
@@ -86,191 +83,191 @@ def count_permutations(event_list):
             elif event[0] == 'sw':
                 permutations.append(num_locations)
         else:
-            if event[0] == 'b':  # baits to specific locations still remove empty buckets
+            if event[0] == 'b': # baits to specific locations still remove empty buckets
                 num_locations -= 1
             permutations.append(1)
     return permutations
 
 
-class ScenarioConfigs():
+class ScenarioConfigs:
+    parameter_space = {
+        # baits and swaps main
+        "visible_baits": [0, 1, 2],  # x
+        "swaps": [0, 1, 2],  # y
+        "visible_swaps": lambda params: range(params['swaps'] + 1),  # z
 
-    def __init__(self):
-        self.all_event_lists = {}
-        self.informed_event_lists = {}
-        self.uninformed_event_lists = {}
-        parameter_space = {
-            # baits and swaps main
-            "visible_baits": [0, 1, 2],  # x
-            "swaps": [0, 1, 2],  # y
-            "visible_swaps": lambda params: range(params['swaps'] + 1),  # z
+        # baits and swaps special
+        "first_swap_is_both": lambda params: [True, False] if params['swaps'] > 0 else [False],
+        "delay_2nd_bait": lambda params: [True, False] if params['swaps'] > 0 and params[
+            'first_swap_is_both'] is False else [False],
+        "second_swap_to_first_loc": lambda params: [True, False] if params['swaps'] == 2 and params[
+            'delay_2nd_bait'] is False else [False],
 
-            # baits and swaps special
-            "first_swap_is_both": lambda params: [True, False] if params['swaps'] > 0 else [False],
-            "delay_2nd_bait": lambda params: [True, False] if params['swaps'] > 0 and params[
-                'first_swap_is_both'] is False else [False],
-            "second_swap_to_first_loc": lambda params: [True, False] if params['swaps'] == 2 and params[
-                'delay_2nd_bait'] is False else [False],
+        # opponent and preferences
+        "num_opponents": [0, 1],
+        # 2+ requires rewrite of most other things, and more parameters
+        "subject_is_dominant": lambda params: [0, 1] if params["num_opponents"] > 0 else [0],
+        # a dominant subject is just as trivial as 0 opponents. There is no need for both.
+        "opponent_prefers_small": lambda params: [0, 1] if params["num_opponents"] > 0 else [0],
+        # we do not also use subject_prefers_small because small here just means different pref, not meaningful size
+        "encourage_sharing": lambda params: [0, 1] if params["num_opponents"] > 0 else [0],
 
-            # opponent and preferences
-            "num_opponents": [0, 1],
-            # 2+ requires rewrite of most other things, and more parameters
-            "subject_is_dominant": lambda params: [0, 1] if params["num_opponents"] > 0 else [0],
-            # a dominant subject is just as trivial as 0 opponents. There is no need for both.
-            "opponent_prefers_small": lambda params: [0, 1] if params["num_opponents"] > 0 else [0],
-            # we do not also use subject_prefers_small because small here just means different pref, not meaningful size
-            "encourage_sharing": lambda params: [0, 1] if params["num_opponents"] > 0 else [0],
+        # baits and swaps ordering (randomized during all training)
+        "first_bait_size": [0, 1],  # i
+        "first_swap_index": lambda params: [0, 1] if params['swaps'] > 0 and params['delay_2nd_bait'] is False and
+                                                     params['first_swap_is_both'] is False else [0],
+        "uninformed_bait": lambda params: [0, 1] if params['visible_baits'] == 1 else [-1],
+        "uninformed_swap": lambda params: [0, 1] if params['swaps'] == 2 and params['visible_swaps'] == 1 else [-1],
+    }
 
-            # baits and swaps ordering (randomized during all training)
-            "first_bait_size": [0, 1],  # i
-            "first_swap_index": lambda params: [0, 1] if params['swaps'] > 0 and params['delay_2nd_bait'] is False and
-                                                         params['first_swap_is_both'] is False else [0],
-            "uninformed_bait": lambda params: [0, 1] if params['visible_baits'] == 1 else [-1],
-            "uninformed_swap": lambda params: [0, 1] if params['swaps'] == 2 and params['visible_swaps'] == 1 else [-1],
-        }
-        for params in parameter_generator(parameter_space):
+    all_event_lists = {}
+    informed_event_lists = {}
+    uninformed_event_lists = {}
+    for params in parameter_generator(parameter_space):
 
-            visible_baits = params['visible_baits']
-            swaps = params['swaps']
-            visible_swaps = params['visible_swaps']
+        visible_baits = params['visible_baits']
+        swaps = params['swaps']
+        visible_swaps = params['visible_swaps']
 
-            first_swap_is_both = params['first_swap_is_both']
-            second_swap_to_first_loc = params['second_swap_to_first_loc']
-            delay_2nd_bait = params['delay_2nd_bait']
+        first_swap_is_both = params['first_swap_is_both']
+        second_swap_to_first_loc = params['second_swap_to_first_loc']
+        delay_2nd_bait = params['delay_2nd_bait']
 
-            opponent_prefers_small = params['opponent_prefers_small']
-            encourage_sharing = params['encourage_sharing']
-            subject_is_dominant = params['subject_is_dominant']
-            num_opponents = params['num_opponents']
+        opponent_prefers_small = params['opponent_prefers_small']
+        encourage_sharing = params['encourage_sharing']
+        subject_is_dominant = params['subject_is_dominant']
+        num_opponents = params['num_opponents']
 
-            first_bait_size = params['first_bait_size']
-            uninformed_bait = params['uninformed_bait']
-            first_swap = params['first_swap_index']
-            uninformed_swap = params['uninformed_swap']
+        first_bait_size = params['first_bait_size']
+        uninformed_bait = params['uninformed_bait']
+        first_swap = params['first_swap_index']
+        uninformed_swap = params['uninformed_swap']
 
-            name = f"b{visible_baits}w{swaps}v{visible_swaps}"
-            name += "f" if first_swap_is_both else ""
-            name += "s" if second_swap_to_first_loc else ""
-            name += "d" if delay_2nd_bait else ""
-            # name += "u" if subject_is_dominant else ""
-            # name += "p" if opponent_prefers_small else ""
-            # name += "h" if encourage_sharing else ""
-            name += "-" + str(
-                1 * first_bait_size + 2 * (uninformed_bait > 0) + 4 * (uninformed_swap > 0) + 8 * (first_swap > 0))
-            events = []
+        name = f"b{visible_baits}w{swaps}v{visible_swaps}"
+        name += "f" if first_swap_is_both else ""
+        name += "s" if second_swap_to_first_loc else ""
+        name += "d" if delay_2nd_bait else ""
+        # name += "u" if subject_is_dominant else ""
+        # name += "p" if opponent_prefers_small else ""
+        # name += "h" if encourage_sharing else ""
+        name += "-" + str(
+            1 * first_bait_size + 2 * (uninformed_bait > 0) + 4 * (uninformed_swap > 0) + 8 * (first_swap > 0))
+        events = []
 
-            bait_index = []
-            for bait_num in range(1 if delay_2nd_bait else 2):
-                bait_size = first_bait_size if bait_num == 0 else 1 - first_bait_size
-                index = add_bait(events, bait_num, bait_size, uninformed_bait, visible_baits)
-                bait_index.append(index)
+        bait_index = []
+        for bait_num in range(1 if delay_2nd_bait else 2):
+            bait_size = first_bait_size if bait_num == 0 else 1 - first_bait_size
+            index = add_bait(events, bait_num, bait_size, uninformed_bait, visible_baits)
+            bait_index.append(index)
 
-            first_swap_index = None
-            for swap_num in range(1 if delay_2nd_bait else swaps):
-                if swap_num == 0 and first_swap_is_both:
-                    first_swap_index = add_swap(events, swap_num, (bait_index[0], bait_index[1]), uninformed_swap,
-                                                visible_swaps)
-                else:
-                    swap_index = bait_index[first_swap] if swap_num == 0 else bait_index[1 - first_swap]
-                    swap_location = first_swap_index if swap_num == 1 and second_swap_to_first_loc else 'e'
-                    first_swap_index = add_swap(events, swap_num, (swap_index, swap_location), uninformed_swap,
-                                                visible_swaps)
-
-            if delay_2nd_bait:
-                bait_size = 1 - first_bait_size
-                index = add_bait(events, 1, bait_size, uninformed_bait, visible_baits, first_swap_index)
-                bait_index.append(index)
-
-            for swap_num in range(1 if delay_2nd_bait else 2, swaps):
+        first_swap_index = None
+        for swap_num in range(1 if delay_2nd_bait else swaps):
+            if swap_num == 0 and first_swap_is_both:
+                first_swap_index = add_swap(events, swap_num, (bait_index[0], bait_index[1]), uninformed_swap,
+                                            visible_swaps)
+            else:
                 swap_index = bait_index[first_swap] if swap_num == 0 else bait_index[1 - first_swap]
                 swap_location = first_swap_index if swap_num == 1 and second_swap_to_first_loc else 'e'
                 first_swap_index = add_swap(events, swap_num, (swap_index, swap_location), uninformed_swap,
                                             visible_swaps)
 
-            events = remove_unnecessary_sequences(events)
+        if delay_2nd_bait:
+            bait_size = 1 - first_bait_size
+            index = add_bait(events, 1, bait_size, uninformed_bait, visible_baits, first_swap_index)
+            bait_index.append(index)
 
-            self.all_event_lists[name] = events
-            if visible_baits == 2 and visible_swaps == swaps:
-                self.informed_event_lists[name] = events
-            if visible_baits == 0 and visible_swaps == 0:
-                self.uninformed_event_lists[name] = events
-            # print(name, events)
+        for swap_num in range(1 if delay_2nd_bait else 2, swaps):
+            swap_index = bait_index[first_swap] if swap_num == 0 else bait_index[1 - first_swap]
+            swap_location = first_swap_index if swap_num == 1 and second_swap_to_first_loc else 'e'
+            first_swap_index = add_swap(events, swap_num, (swap_index, swap_location), uninformed_swap, visible_swaps)
 
-        '''lack_to_generalized = {
-            "moved": "0.2.2",
-            "partiallyUninformed": "1.0.0",
-            "replaced": "1.1.0d",
-            "informedControl": "2.0.0",
-            "removedUninformed": "2.1.0",
-            "swapped": "2.1.0a",
-            "removedInformed": "2.1.1",
-            "misinformed": "2.2.0c",
-            "misinformed2": "2.2.0ac",
-        }
-    
-        matching_names = {}
-        for key, value in lack_to_generalized.items():
-            matching_names[key] = []
-            for name in all_event_lists.keys():
-                if name.startswith(value + "-"):
-                    matching_names[key].append(name)
-    
-        for key, value in matching_names.items():
-            print(f"For {key}, found matches: {value}")'''
+        events = remove_unnecessary_sequences(events)
 
-        print('total lists', len(self.all_event_lists), 'informed lists', len(self.informed_event_lists), 'uninformed lists',
-              len(self.uninformed_event_lists))
+        all_event_lists[name] = events
+        if visible_baits == 2 and visible_swaps == swaps:
+            informed_event_lists[name] = events
+        if visible_baits == 0 and visible_swaps == 0:
+            uninformed_event_lists[name] = events
+        # print(name, events)
 
-        all_event_delays = {}
-        total_fillers = 0
+    '''lack_to_generalized = {
+        "moved": "0.2.2",
+        "partiallyUninformed": "1.0.0",
+        "replaced": "1.1.0d",
+        "informedControl": "2.0.0",
+        "removedUninformed": "2.1.0",
+        "swapped": "2.1.0a",
+        "removedInformed": "2.1.1",
+        "misinformed": "2.2.0c",
+        "misinformed2": "2.2.0ac",
+    }
 
-        for name, listy in self.all_event_lists.items():
-            non_ob = count_non_ob_re(listy)
-            fillers = list(generate_fillers(8 - (len(listy) - non_ob), non_ob))
-            all_event_delays[name] = fillers
-            total_fillers += len(fillers)
+    matching_names = {}
+    for key, value in lack_to_generalized.items():
+        matching_names[key] = []
+        for name in all_event_lists.keys():
+            if name.startswith(value + "-"):
+                matching_names[key].append(name)
 
-        all_event_permutations = {}
-        total_products = 0
-        for event_name in self.all_event_lists:
-            all_event_permutations[event_name] = count_permutations(self.all_event_lists[event_name])
-            product = np.product(all_event_permutations[event_name])
-            total_products += product * len(all_event_delays[event_name])
-        print('total fillers', total_fillers, 'total permutations', total_products)
+    for key, value in matching_names.items():
+        print(f"For {key}, found matches: {value}")'''
 
-        # generate 'stages' for train and test, where a stage is a list of event lists and parameters
-        self.stages = {
-            'a0': {'events': self.all_event_lists, 'params': 'no-op'},
-            'i0': {'events': self.informed_event_lists, 'params': 'no-op'},
-            'u0': {'events': self.uninformed_event_lists, 'params': 'no-op'},
+    print('total lists', len(all_event_lists), 'informed lists', len(informed_event_lists), 'uninformed lists',
+          len(uninformed_event_lists))
 
-            'i1': {'events': self.informed_event_lists, 'params': 'defaults'},
-            'u1': {'events': self.uninformed_event_lists, 'params': 'defaults'},
-            'a1': {'events': self.all_event_lists, 'params': 'defaults'},
-        }
 
-        self.standoff = {
-            "defaults": {
-                "deterministic": True,
-                "hidden": True,
-                "share_rewards": False,
-                "boxes": 5,
-                "sub_valence": 1,
-                "dom_valence": 1,
-                "subject_is_dominant": False,
-                "num_puppets": 1,
-                "num_agents": 1,
-                "events": [[['bait', 'empty'], ['bait', 'empty']]]
-            },
-            "no-op": {
-                "deterministic": True,
-                "hidden": True,
-                "share_rewards": False,
-                "boxes": 5,
-                "sub_valence": 1,
-                "dom_valence": 1,
-                "subject_is_dominant": False,
-                "num_puppets": 0,
-                "num_agents": 1,
-                "events": [[['bait', 'empty'], ['bait', 'empty']]]
-            },
-        }
+    all_event_delays = {}
+    total_fillers = 0
+
+    for name, listy in all_event_lists.items():
+        non_ob = count_non_ob_re(listy)
+        fillers = list(generate_fillers(8 - (len(listy) - non_ob), non_ob))
+        all_event_delays[name] = fillers
+        total_fillers += len(fillers)
+
+    all_event_permutations = {}
+    total_products = 0
+    for event_name in all_event_lists:
+        all_event_permutations[event_name] = count_permutations(all_event_lists[event_name])
+        product = np.product(all_event_permutations[event_name])
+        total_products += product*len(all_event_delays[event_name])
+    print('total fillers', total_fillers, 'total permutations', total_products)
+
+    # generate 'stages' for train and test, where a stage is a list of event lists and parameters
+    stages = {
+        'a0': {'events': all_event_lists, 'params': 'no-op'},
+        'i0': {'events': informed_event_lists, 'params': 'no-op'},
+        'u0': {'events': uninformed_event_lists, 'params': 'no-op'},
+
+        'i1': {'events': informed_event_lists, 'params': 'defaults'},
+        'u1': {'events': uninformed_event_lists, 'params': 'defaults'},
+        'a1': {'events': all_event_lists, 'params': 'defaults'},
+    }
+
+
+    standoff = {
+        "defaults": {
+            "deterministic": True,
+            "hidden": True,
+            "share_rewards": False,
+            "boxes": 5,
+            "sub_valence": 1,
+            "dom_valence": 1,
+            "subject_is_dominant": False,
+            "num_puppets": 1,
+            "num_agents": 1,
+            "events": [[['bait', 'empty'], ['bait', 'empty']]]
+        },
+        "no-op": {
+            "deterministic": True,
+            "hidden": True,
+            "share_rewards": False,
+            "boxes": 5,
+            "sub_valence": 1,
+            "dom_valence": 1,
+            "subject_is_dominant": False,
+            "num_puppets": 0,
+            "num_agents": 1,
+            "events": [[['bait', 'empty'], ['bait', 'empty']]]
+        },
+    }
