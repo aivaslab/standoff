970e7ce2cd3fad3532e5e664dd47105642dcc84a
Joel Michelson <joelmichelson@users.noreply.github.com>
2023-07-27 01:05:42 -0500

    Update supervised_learning.py

--- src/supervised_learning.py
+++ src/supervised_learning.py
@@ -207,7 +207,7 @@ def gen_data(labels=[], path='supervised', pref_type='', role_type='', record_ex
         for label in labels:
             write_to_h5py(np.array(data_labels[label]), os.path.join(this_path,'label-' + label + '.h5'), key='data')'''
 
-        sparse_data = [pickle.dumps(sparse.csr_matrix(datapoint)) for datapoint in data]
+        sparse_data = [pickle.dumps(sparse.csr_matrix(datapoint)) for datapoint in data_obs]
         np.savez_compressed(os.path.join(this_path, 'obs'), np.array(sparse_data))
         np.savez_compressed(os.path.join(this_path,  'params'), np.array(data_params))
         for label in labels:
